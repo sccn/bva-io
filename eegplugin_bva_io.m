1a0e3056d9b8b8757051b2912909a8dd22895ee0
widmann <widmann@uni-leipzig.de>
2020-08-19 17:07:02 +0200

    Version number

--- eegplugin_bva_io.m
+++ eegplugin_bva_io.m
@@ -34,7 +34,7 @@
 
 function vers = eegplugin_bva_io(fig, trystrs, catchstrs)
 
-    vers = 'bva_io1.5.14';
+    vers = 'bva_io1.6';
     if nargin < 3
         error('eegplugin_bva_io requires 3 arguments');
     end;
