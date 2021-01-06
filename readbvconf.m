873c043120d40546eb974419faccc14b28fdd7fa
Arnaud Delorme <arnodelorme@gmail.com>
2021-01-05 17:09:44 -1000

    version 1.7

--- readbvconf.m
+++ readbvconf.m
@@ -36,24 +36,31 @@ function CONF = readbvconf(pathname, filename)
 if nargin < 2
     error('Not enough input arguments');
 end
+% 
+% % Open and read file (old method, much slower)
+% [IN, message] = fopen(fullfile(pathname, filename), 'r');
+% if IN == -1
+%     [IN, message] = fopen(fullfile(pathname, lower(filename)));
+%     if IN == -1
+%         error(message)
+%     end;
+% end
+% raw={};
+% while ~feof(IN)
+%     raw = [raw; {fgetl(IN)}];
+% end
+% fclose(IN);
+% 
+% % Remove comments and empty lines
+% raw(cellfun('isempty', raw) == true) = [];
+% raw(strmatch(';', raw)) = [];
 
-% Open and read file
-[IN, message] = fopen(fullfile(pathname, filename), 'r');
-if IN == -1
-    [IN, message] = fopen(fullfile(pathname, lower(filename)));
-    if IN == -1
-        error(message)
-    end;
-end
-raw={};
-while ~feof(IN)
-    raw = [raw; {fgetl(IN)}];
-end
-fclose(IN);
-
-% Remove comments and empty lines
-raw(strmatch(';', raw)) = [];
-raw(cellfun('isempty', raw) == true) = [];
+% Open and read file (automatically remove empty lines)
+fid = fopen(fullfile(pathname, filename), 'r');
+raw = textscan(fid, '%s', 'delimiter', '');
+fclose(fid);
+raw = raw{1};
+raw(strmatch(';', raw)) = []; % remove comments
 
 % Find sections
 sectionArray = [strmatch('[', raw)' length(raw) + 1];
@@ -70,11 +77,10 @@ for iSection = 1:length(sectionArray) - 1
         case {'commoninfos' 'binaryinfos' 'asciiinfos'}
             for line = sectionArray(iSection) + 1:sectionArray(iSection + 1) - 1
                 splitArray = strfind(raw{line}, '=');
-                if ~isvarname( lower(raw{line}(1:splitArray(1) - 1)) )
-                    warning( [ 'Non-standard-conforming line ''' raw{ line } ''' found. Skipping. Please report to author of exporting software.' ] )
-                else
-                    CONF.(fieldName).(lower(raw{line}(1:splitArray(1) - 1))) = raw{line}(splitArray(1) + 1:end);
-                end
+                fieldName2  = lower(raw{line}(1:splitArray(1) - 1));
+                fieldName2(fieldName2 == ' ') = '_';
+                fieldName2(fieldName2 == ':') = '_';
+                CONF.(fieldName).(fieldName2) = raw{line}(splitArray(1) + 1:end);
             end
         case {'channelinfos' 'coordinates'}
             for line = sectionArray(iSection) + 1:sectionArray(iSection + 1) - 1
@@ -82,13 +88,38 @@ for iSection = 1:length(sectionArray) - 1
                 CONF.(fieldName)(str2double(raw{line}(3:splitArray(1) - 1))) = {raw{line}(splitArray(1) + 1:end)};
             end
         case {'markerinfos'} % Allow discontinuity for markers (but not channelinfos and coordinates!)
-            for line = sectionArray(iSection) + 1:sectionArray(iSection + 1) - 1
-                splitArray = strfind(raw{line}, '=');
-                CONF.(fieldName)(line - sectionArray(iSection), :) = {raw{line}(splitArray(1) + 1:end) str2double(raw{line}(3:splitArray(1) - 1))};
+            % try reading the whole section at once
+            
+            try
+                fileData = raw(sectionArray(iSection) + 1:sectionArray(iSection + 1) - 1);
+                fileData = sprintf('%s\n', fileData{:});
+                newData = textscan(fileData', '%s', 'delimiter', '=');
+                newData = newData{1};
+                eventValues  = newData(2:2:end);
+                eventMarkers = newData(1:2:end-1);
+                eventMarkers = cellfun(@(x)str2double(x(3:end)), eventMarkers, 'uniformoutput', false);
+                markerInfo = eventValues;
+                markerInfo(:,2) = eventMarkers;
+                CONF.( [ fieldName ] ) = markerInfo;
+            catch
+                % old method (much slower)
+                for line = sectionArray(iSection) + 1:sectionArray(iSection + 1) - 1
+                    splitArray = strfind(raw{line}, '=');
+                    CONF.(fieldName)(line - sectionArray(iSection), :) = {raw{line}(splitArray(1) + 1:end) str2double(raw{line}(3:splitArray(1) - 1))};
+                end
             end
-            if ~all(1:size(CONF.(fieldName), 1) == [CONF.(fieldName){:, 2}])
-                warning('Marker number discontinuity.')
+            
+            if ~isfield(CONF, fieldName)
+                disp('No event found');
+            else
+                if any(cellfun(@isempty, CONF.(fieldName)(:,1)))
+                    warning('Empty event(s).')
+                end                        
+                if ~all(1:size(CONF.(fieldName), 1) == [CONF.(fieldName){:, 2}])
+                    warning('Marker number discontinuity.')
+                end
             end
+            
         case 'comment'
             CONF.(fieldName) = raw(sectionArray(iSection) + 1:sectionArray(iSection + 1) - 1);
         otherwise
@@ -97,7 +128,7 @@ for iSection = 1:length(sectionArray) - 1
 end
 
 % Handle ahdr file type exceptions
-[ ~, ~, ext ] = fileparts( filename );
+[ ~, ~, ext ] = fileparts( fullfile(pathname, filename) );
 if strcmp( ext, '.ahdr' )
     
     if ~isfield( CONF.commoninfos, 'numberofchannels' )
@@ -112,3 +143,5 @@ if strcmp( ext, '.ahdr' )
     CONF.coordinates(ahdrChan) = { '0,0,0' };
     
 end
+
+disp('Done.')
