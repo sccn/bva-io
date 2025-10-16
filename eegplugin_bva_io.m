5f08aa22a7b4590b8bcdbd6251c20b975c202603
widmann <widmann@uni-leipzig.de>
2025-10-16 12:14:21 +0200

    Release 1.74

--- eegplugin_bva_io.m
+++ eegplugin_bva_io.m
@@ -34,7 +34,7 @@
 
 function vers = eegplugin_bva_io(fig, trystrs, catchstrs)
 
-    vers = 'bva_io1.73';
+    vers = 'bva_io1.74';
     if nargin < 3
         error('eegplugin_bva_io requires 3 arguments');
     end;
